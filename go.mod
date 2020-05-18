7b2294b5798108d6f2f001f535725127268017c9
Hiroshi Muraoka <h.muraoka714@gmail.com>
2020-05-18 06:48:11 +0000

    Exec "kubebuilder create api --group myso --version v1alpha1 --kind MySQLCluster"
    Signed-off-by: Hiroshi Muraoka <h.muraoka714@gmail.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/cybozu-go/myso
 go 1.13
 
 require (
+	github.com/go-logr/logr v0.1.0
 	k8s.io/apimachinery v0.17.2
 	k8s.io/client-go v0.17.2
 	sigs.k8s.io/controller-runtime v0.5.0
--- main.go
+++ main.go
@@ -25,6 +25,9 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	mysov1alpha1 "github.com/cybozu-go/myso/api/v1alpha1"
+	"github.com/cybozu-go/myso/controllers"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -36,6 +39,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = mysov1alpha1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -62,6 +66,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.MySQLClusterReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("MySQLCluster"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "MySQLCluster")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
