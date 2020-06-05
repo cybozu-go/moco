13f027cf95c8a1b2f0c9d7c0efbfc179ffddd13e
binoue <banji-inoue@cybozu.co.jp>
2020-06-05 17:02:09 +0900

    delete unnecessary import

--- cmd/moco-controller/cmd/run.go
+++ cmd/moco-controller/cmd/run.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
