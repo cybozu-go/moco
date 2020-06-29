2a486260ef2211ea242edd6cae50c83373045f80
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2020-06-29 18:17:07 +0900

    Add comments to the MyConfParameters type
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- types.go
+++ types.go
@@ -1,6 +1,9 @@
 package moco
 
+// MyConfTemplateParameters define parameters for a MySQL configuration template
 type MyConfTemplateParameters struct {
-	ServerID     uint
+	// ServerID is the value for server_id of MySQL configuration
+	ServerID uint
+	// AdminAddress is the value for admin_address of MySQL configuration
 	AdminAddress string
 }
