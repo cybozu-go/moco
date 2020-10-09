d23ab02892098f8a306b3465462ff84d9218749b
Kazuhito MATSUDA <kazuhito.matuda@gmail.com>
2020-10-09 16:39:50 +0900

    Tests for getting intermediate primary options.
    Signed-off-by: Kazuhito MATSUDA <kazuhito.matuda@gmail.com>

--- accessor/mysql_status_test.go
+++ accessor/mysql_status_test.go
@@ -11,42 +11,33 @@ import (
 	"github.com/jmoiron/sqlx"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/uuid"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 const (
-	host     = "localhost"
-	password = "test-password"
-	port     = 3306
+	host      = "localhost"
+	password  = "test-password"
+	port      = 3306
+	namespace = "test-namespace"
 )
 
+var intermediateSecret = "intermediate-primary-secret"
+
 var _ = Describe("Get MySQLCluster status", func() {
-	It("Should get MySQL status", func() {
+	It("should initialize MySQL for testing", func() {
 		err := initializeMySQL()
 		Expect(err).ShouldNot(HaveOccurred())
-		acc := NewMySQLAccessor(&MySQLAccessorConfig{
-			ConnMaxLifeTime:   30 * time.Minute,
-			ConnectionTimeout: 3 * time.Second,
-			ReadTimeout:       30 * time.Second,
-		})
+	})
 
-		inf := NewInfrastructure(k8sClient, acc, password, []string{host}, 3306)
-		cluster := mocov1alpha1.MySQLCluster{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:        "test",
-				ClusterName: "test-cluster",
-				Namespace:   "test-namespace",
-				UID:         "test-uid",
-			},
-			Spec: mocov1alpha1.MySQLClusterSpec{
-				Replicas: 1,
-			},
-		}
+	It("should get MySQL status", func() {
+		_, inf, cluster := getAccessorInfraCluster()
+
 		logger := ctrl.Log.WithName("controllers").WithName("MySQLCluster")
-
 		sts := GetMySQLClusterStatus(context.Background(), logger, inf, &cluster)
+
 		Expect(sts.InstanceStatus).Should(HaveLen(1))
 		Expect(sts.InstanceStatus[0].PrimaryStatus).ShouldNot(BeNil())
 		Expect(sts.InstanceStatus[0].ReplicaStatus).ShouldNot(BeNil())
@@ -54,6 +45,71 @@ var _ = Describe("Get MySQLCluster status", func() {
 		Expect(sts.InstanceStatus[0].GlobalVariablesStatus).ShouldNot(BeNil())
 		Expect(sts.InstanceStatus[0].CloneStateStatus).ShouldNot(BeNil())
 		Expect(*sts.Latest).Should(Equal(0))
+	})
+
+	It("should get and validate intermediate primary options", func() {
+		_, inf, cluster := getAccessorInfraCluster()
+		cluster.Spec.ReplicationSourceSecretName = &intermediateSecret
+		err := k8sClient.Create(context.Background(), &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}})
+		Expect(err).ShouldNot(HaveOccurred())
+
+		By("setting valid options to api server")
+		data := map[string]string{
+			"MASTER_HOST": "dummy-master",
+			"MASTER_PORT": "3306",
+		}
+		var ipSecret corev1.Secret
+		ipSecret.ObjectMeta.Name = intermediateSecret
+		ipSecret.ObjectMeta.Namespace = namespace
+		ipSecret.Data = make(map[string][]byte)
+		for k, v := range data {
+			ipSecret.Data[k] = []byte(v)
+		}
+		err = k8sClient.Create(context.Background(), &ipSecret)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		By("getting and validating intermediate primary options")
+		logger := ctrl.Log.WithName("controllers").WithName("MySQLCluster")
+		sts := GetMySQLClusterStatus(context.Background(), logger, inf, &cluster)
+		Expect(sts.IntermediatePrimaryOptions).Should(Equal(data))
+
+		By("setting options without MASTER_HOST to api server")
+		data = map[string]string{
+			"MASTER_PORT": "3306",
+		}
+		ipSecret.ObjectMeta.Name = intermediateSecret
+		ipSecret.ObjectMeta.Namespace = namespace
+		ipSecret.Data = make(map[string][]byte)
+		for k, v := range data {
+			ipSecret.Data[k] = []byte(v)
+		}
+		err = k8sClient.Update(context.Background(), &ipSecret)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		By("getting and validating intermediate primary options")
+		logger = ctrl.Log.WithName("controllers").WithName("MySQLCluster")
+		sts = GetMySQLClusterStatus(context.Background(), logger, inf, &cluster)
+		Expect(sts.IntermediatePrimaryOptions).Should(BeNil())
+
+		By("setting options without INVALID_OPTION to api server")
+		data = map[string]string{
+			"MASTER_HOST":    "dummy-master",
+			"MASTER_PORT":    "3306",
+			"INVALID_OPTION": "invalid",
+		}
+		ipSecret.ObjectMeta.Name = intermediateSecret
+		ipSecret.ObjectMeta.Namespace = namespace
+		ipSecret.Data = make(map[string][]byte)
+		for k, v := range data {
+			ipSecret.Data[k] = []byte(v)
+		}
+		err = k8sClient.Update(context.Background(), &ipSecret)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		By("getting and validating intermediate primary options")
+		logger = ctrl.Log.WithName("controllers").WithName("MySQLCluster")
+		sts = GetMySQLClusterStatus(context.Background(), logger, inf, &cluster)
+		Expect(sts.IntermediatePrimaryOptions).Should(BeNil())
 	})
 })
 
@@ -117,4 +173,26 @@ func initializeMySQL() error {
 	}
 
 	return nil
+}
+
+func getAccessorInfraCluster() (*MySQLAccessor, Infrastructure, mocov1alpha1.MySQLCluster) {
+	acc := NewMySQLAccessor(&MySQLAccessorConfig{
+		ConnMaxLifeTime:   30 * time.Minute,
+		ConnectionTimeout: 3 * time.Second,
+		ReadTimeout:       30 * time.Second,
+	})
+	inf := NewInfrastructure(k8sClient, acc, password, []string{host}, 3306)
+	cluster := mocov1alpha1.MySQLCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "test",
+			ClusterName: "test-cluster",
+			Namespace:   namespace,
+			UID:         "test-uid",
+		},
+		Spec: mocov1alpha1.MySQLClusterSpec{
+			Replicas: 1,
+		},
+	}
+
+	return acc, inf, cluster
 }
