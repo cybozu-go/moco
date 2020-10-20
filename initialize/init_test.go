908b85c249eaa7828aebab965187989cb0c43328
zoetrope <a.ikezoe@gmail.com>
2020-10-20 17:02:32 +0900

    Confirm initializeInstance
    Signed-off-by: zoetrope <a.ikezoe@gmail.com>

--- initialize/init_test.go
+++ initialize/init_test.go
@@ -2,13 +2,41 @@ package initialize
 
 import (
 	"context"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/cybozu-go/moco"
 )
 
 func testInitializeInstance(t *testing.T) {
-	err := initializeInstance(context.Background())
+	err := os.MkdirAll(moco.MySQLConfPath, 0755)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	confPath := filepath.Join(moco.MySQLConfPath, moco.MySQLConfName)
+	err = ioutil.WriteFile(confPath, []byte(`[client]
+loose_default_character_set = utf8mb4
+[mysqld]
+socket = /var/run/mysqld/mysqld.sock
+datadir = /var/lib/mysql
+log_error = /var/log/mysql/error.log
+slow_query_log_file = /var/log/mysql/slow.log
+pid_file = /var/run/mysqld/mysqld.pid
+character_set_server = utf8mb4
+collation_server = utf8mb4_unicode_ci
+default_time_zone = +0:00
+disabled_storage_engines = MyISAM
+enforce_gtid_consistency = ON
+gtid_mode = ON
+`), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = initializeInstance(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
