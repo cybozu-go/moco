1157af54cc96793f6d601a7aef43950c45d97315
zoetrope <a.ikezoe@gmail.com>
2020-10-27 12:07:51 +0900

    Add mysqlConnector for e2e-test
    Signed-off-by: zoetrope <a.ikezoe@gmail.com>

--- e2e/bootstrap_test.go
+++ e2e/bootstrap_test.go
@@ -4,60 +4,41 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/cybozu-go/moco/api/v1alpha1"
+	"github.com/cybozu-go/moco"
+	"github.com/jmoiron/sqlx"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 )
 
 func testBootstrap() {
-	It("should create StatefulSet", func() {
+	It("should create cluster", func() {
 		By("registering MySQLCluster")
-		kubectl("create", "ns", "e2e-test") // ignore error
+		_, _, _ = kubectl("create", "ns", "e2e-test") // ignore error
 		stdout, stderr, err := kubectl("apply", "-n", "e2e-test", "-f", "manifests/mysql_cluster.yaml")
 		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
 
 		By("getting Secret which contains root password")
 		Eventually(func() error {
-			stdout, stderr, err := kubectl("get", "-n", "e2e-test", "mysqlcluster/mysqlcluster", "-o", "json")
+			mysqlCluster, err := getMySQLCluster()
 			if err != nil {
-				return fmt.Errorf("failed to get MySQLCluster. stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+				return err
 			}
-
-			var mysqlCluster v1alpha1.MySQLCluster
-			err = json.Unmarshal(stdout, &mysqlCluster)
+			_, err = getRootPassword(mysqlCluster)
 			if err != nil {
-				return fmt.Errorf("failed to unmarshal MySQLCluster. stdout: %s, err: %v", stdout, err)
+				return err
 			}
-			stdout, stderr, err = kubectl("get", "-n", "e2e-test", "secret/root-password-mysqlcluster-"+string(mysqlCluster.GetUID()), "-o", "json")
-			if err != nil {
-				return fmt.Errorf("failed to get Secret. stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
-			}
-
-			var secret corev1.Secret
-			err = json.Unmarshal(stdout, &secret)
-			if err != nil {
-				return fmt.Errorf("failed to unmarshal Secret stdout: %s, err: %v", stdout, err)
-			}
-
 			return nil
 		}).Should(Succeed())
 
 		By("getting StatefulSet")
 		Eventually(func() error {
-			stdout, stderr, err := kubectl("get", "-n", "e2e-test", "mysqlcluster/mysqlcluster", "-o", "json")
+			mysqlCluster, err := getMySQLCluster()
 			if err != nil {
-				return fmt.Errorf("failed to get MySQLCluster. stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+				return err
 			}
 
-			var mysqlCluster v1alpha1.MySQLCluster
-			err = json.Unmarshal(stdout, &mysqlCluster)
-			if err != nil {
-				return fmt.Errorf("failed to unmarshal MySQLCluster. stdout: %s, err: %v", stdout, err)
-			}
-
-			stdout, stderr, err = kubectl("get", "-n", "e2e-test", "statefulsets/mysqlcluster-"+string(mysqlCluster.GetUID()), "-o", "json")
+			stdout, stderr, err = kubectl("get", "-n", "e2e-test", "statefulsets", moco.UniqueName(mysqlCluster), "-o", "json")
 			if err != nil {
 				return fmt.Errorf("failed to get StatefulSet. stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
 			}
@@ -77,5 +58,47 @@ func testBootstrap() {
 			}
 			return nil
 		}).Should(Succeed())
+	})
+
+	It("should replicate data", func() {
+		mysqlCluster, err := getMySQLCluster()
+		Expect(err).ShouldNot(HaveOccurred())
+		connector := newMySQLConnector(mysqlCluster)
+		err = connector.startPortForward()
+		Expect(err).ShouldNot(HaveOccurred())
+		defer connector.stopPortForward()
+
+		var primaryDB *sqlx.DB
+		Eventually(func() error {
+			primaryDB, err = connector.connectToPrimary()
+			if err != nil {
+				return err
+			}
+			return nil
+		}).Should(Succeed())
+		var replicaDB *sqlx.DB
+		Eventually(func() error {
+			replicaDB, err = connector.connect(1)
+			if err != nil {
+				return err
+			}
+			return nil
+		}).Should(Succeed())
+		Expect(replicaDB).ShouldNot(BeNil())
+
+		_, err = primaryDB.Exec("CREATE DATABASE IF NOT EXISTS test")
+		Expect(err).ShouldNot(HaveOccurred())
+
+		_, err = primaryDB.Exec(`CREATE TABLE IF NOT EXISTS test.t1 (
+num bigint unsigned NOT NULL AUTO_INCREMENT,
+val0 varchar(100) DEFAULT NULL,
+val1 varchar(100) DEFAULT NULL,
+val2 varchar(100) DEFAULT NULL,
+val3 varchar(100) DEFAULT NULL,
+val4 varchar(100) DEFAULT NULL,
+  UNIQUE KEY num (num)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+`)
+		Expect(err).ShouldNot(HaveOccurred())
 	})
 }
--- e2e/run_test.go
+++ e2e/run_test.go
@@ -2,7 +2,17 @@ package e2e
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"os/exec"
+	"time"
+
+	"github.com/cybozu-go/moco"
+	"github.com/cybozu-go/moco/accessor"
+	mocov1alpha1 "github.com/cybozu-go/moco/api/v1alpha1"
+	"github.com/jmoiron/sqlx"
+	corev1 "k8s.io/api/core/v1"
 )
 
 func execAtLocal(cmd string, input []byte, args ...string) ([]byte, []byte, error) {
@@ -28,12 +38,90 @@ func kubectlWithInput(input []byte, args ...string) ([]byte, []byte, error) {
 	return execAtLocal("./bin/kubectl", input, args...)
 }
 
-//lint:ignore U1000 This func may be used in the future.
-func containString(s []string, target string) bool {
-	for _, ss := range s {
-		if ss == target {
-			return true
-		}
+func getMySQLCluster() (*mocov1alpha1.MySQLCluster, error) {
+	stdout, stderr, err := kubectl("get", "-n", "e2e-test", "mysqlcluster", "mysqlcluster", "-o", "json")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get MySQLCluster. stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
 	}
-	return false
+
+	var mysqlCluster mocov1alpha1.MySQLCluster
+	err = json.Unmarshal(stdout, &mysqlCluster)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal MySQLCluster. stdout: %s, err: %v", stdout, err)
+	}
+	return &mysqlCluster, nil
+}
+
+func getRootPassword(mysqlCluster *mocov1alpha1.MySQLCluster) (*corev1.Secret, error) {
+	stdout, stderr, err := kubectl("get", "-n", "e2e-test", "secret", "root-password-"+moco.UniqueName(mysqlCluster), "-o", "json")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Secret. stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+	}
+
+	var secret corev1.Secret
+	err = json.Unmarshal(stdout, &secret)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal Secret stdout: %s, err: %v", stdout, err)
+	}
+	return &secret, nil
+}
+
+type mysqlConnector struct {
+	cluster             *mocov1alpha1.MySQLCluster
+	portForwardCommands []*exec.Cmd
+	accessor            *accessor.MySQLAccessor
+	basePort            int
+}
+
+func newMySQLConnector(cluster *mocov1alpha1.MySQLCluster) *mysqlConnector {
+	acc := accessor.NewMySQLAccessor(&accessor.MySQLAccessorConfig{
+		ConnMaxLifeTime:   30 * time.Minute,
+		ConnectionTimeout: 3 * time.Second,
+		ReadTimeout:       30 * time.Second,
+	})
+	return &mysqlConnector{
+		cluster:  cluster,
+		accessor: acc,
+		basePort: 13306,
+	}
+}
+
+func (c *mysqlConnector) startPortForward() error {
+	for i := 0; i < int(c.cluster.Spec.Replicas); i++ {
+		podName := fmt.Sprintf("%s-%d", moco.UniqueName(c.cluster), i)
+		port := c.basePort + i
+		command := exec.Command("./bin/kubectl", "-n", "e2e-test", "port-forward", "pod/"+podName, fmt.Sprintf("%d:%d", port, moco.MySQLPort))
+		err := command.Start()
+		if err != nil {
+			c.stopPortForward()
+			return err
+		}
+		c.portForwardCommands = append(c.portForwardCommands, command)
+	}
+	return nil
+}
+
+func (c *mysqlConnector) stopPortForward() {
+	for _, command := range c.portForwardCommands {
+		_ = command.Process.Kill()
+	}
+	c.portForwardCommands = []*exec.Cmd{}
+}
+
+func (c *mysqlConnector) connectToPrimary() (*sqlx.DB, error) {
+	if c.cluster.Status.CurrentPrimaryIndex == nil {
+		return nil, errors.New("CurrentPrimaryIndex is nil")
+	}
+	return c.connect(*c.cluster.Status.CurrentPrimaryIndex)
+}
+
+func (c *mysqlConnector) connect(index int) (*sqlx.DB, error) {
+	port := c.basePort + index
+	addr := fmt.Sprintf("127.0.0.1:%d", port)
+	secret, err := getRootPassword(c.cluster)
+	if err != nil {
+		return nil, err
+	}
+	password := string(secret.Data[moco.OperatorPasswordEnvName])
+	return c.accessor.Get(addr, moco.OperatorAdminUser, password)
 }
