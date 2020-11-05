60bc9a7fef6c83b2e70a67f3d0bb8e79e7c333ac
Kazuhito MATSUDA <kazuhito.matuda@gmail.com>
2020-11-05 14:18:54 +0900

    Experimental change.
    Signed-off-by: Kazuhito MATSUDA <kazuhito.matuda@gmail.com>

--- controllers/mysql_clustering.go
+++ controllers/mysql_clustering.go
@@ -166,7 +166,14 @@ func decideNextOperation(log logr.Logger, cluster *mocov1alpha1.MySQLCluster, st
 
 	op = restoreEmptyInstance(status, cluster)
 	if len(op) != 0 {
+		var wait bool
+		for _, o := range op {
+			if o.Name() == ops.OperatorClone {
+				wait = true
+			}
+		}
 		return &Operation{
+			Wait:      wait,
 			Operators: op,
 			Phase:     moco.PhaseRestoreInstance,
 			Event:     &moco.EventRestoringReplicaInstances,
@@ -480,7 +487,11 @@ func isCloneable(s *accessor.MySQLInstanceStatus) bool {
 }
 
 func isCloning(state sql.NullString) bool {
-	return state.String == moco.CloneStatusNotStarted || state.String == moco.CloneStatusInProgress
+	if !state.Valid {
+		return false
+	}
+
+	return state.String != moco.CloneStatusCompleted
 }
 
 func restoreEmptyInstance(status *accessor.MySQLClusterStatus, cluster *mocov1alpha1.MySQLCluster) []ops.Operator {
