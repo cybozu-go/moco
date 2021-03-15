7e70b555f890192c7c33d022d9a8d57d2a1d3044
Kazuhito MATSUDA <kazuhito.matuda@gmail.com>
2021-03-15 17:30:14 +0900

    Remove RootPasswordSecretName from MySQLCLuster CRD.
    Signed-off-by: Kazuhito MATSUDA <kazuhito.matuda@gmail.com>

--- api/v1alpha1/zz_generated.deepcopy.go
+++ api/v1alpha1/zz_generated.deepcopy.go
@@ -106,11 +106,6 @@ func (in *MySQLClusterSpec) DeepCopyInto(out *MySQLClusterSpec) {
 		*out = new(string)
 		**out = **in
 	}
-	if in.RootPasswordSecretName != nil {
-		in, out := &in.RootPasswordSecretName, &out.RootPasswordSecretName
-		*out = new(string)
-		**out = **in
-	}
 	if in.ReplicationSourceSecretName != nil {
 		in, out := &in.ReplicationSourceSecretName, &out.ReplicationSourceSecretName
 		*out = new(string)
