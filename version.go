76c35f45cb3ad758c74dfbb0a700f6bcde86277e
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2021-07-07 18:16:37 +0900

    Bump version to 0.10.4

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package moco
 
 const (
 	// Version is the MOCO version
-	Version = "0.10.3"
+	Version = "0.10.4"
 
 	// FluentBitImage is the image for slow-log sidecar container.
 	FluentBitImage = "quay.io/cybozu/fluent-bit:1.7.8.1"
