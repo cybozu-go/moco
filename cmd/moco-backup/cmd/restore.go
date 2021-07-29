fcde2030b71a9057368807dbb6c2c9db2a8a10e8
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2021-07-29 14:37:23 +0900

    Retry restore process
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- cmd/moco-backup/cmd/restore.go
+++ cmd/moco-backup/cmd/restore.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/cybozu-go/moco/backup"
@@ -23,38 +24,62 @@ NAME:             The target MySQLCluster's name.
 YYYYMMDD-hhmmss:  The point-in-time to restore data.  e.g. 20210523-150423`,
 	Args: cobra.ExactArgs(6),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		bucketName := args[0]
-		srcNamespace := args[1]
-		srcName := args[2]
-		namespace := args[3]
-		name := args[4]
+		maxRetry := 3
+		for i := 0; i < maxRetry; i++ {
+			if err := runRestore(cmd, args); err != backup.ErrBadConnection {
+				return err
+			}
 
-		restorePoint, err := time.Parse(constants.BackupTimeFormat, args[5])
-		if err != nil {
-			return fmt.Errorf("invalid restore point %s: %w", args[5], err)
+			fmt.Fprintf(os.Stderr, "bad connection: retrying...\n")
+			time.Sleep(1 * time.Second)
 		}
 
-		b, err := makeBucket(bucketName)
-		if err != nil {
-			return fmt.Errorf("failed to create a bucket interface: %w", err)
-		}
-
-		cfg, err := ctrl.GetConfig()
-		if err != nil {
-			return fmt.Errorf("failed to get config for Kubernetes: %w", err)
-		}
-
-		rm, err := backup.NewRestoreManager(cfg, b, commonArgs.workDir,
-			srcNamespace, srcName,
-			namespace, name,
-			mysqlPassword,
-			commonArgs.threads,
-			restorePoint)
-		if err != nil {
-			return fmt.Errorf("failed to create a restore manager: %w", err)
-		}
-		return rm.Restore(cmd.Context())
+		return nil
 	},
+}
+
+func runRestore(cmd *cobra.Command, args []string) (e error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if r == backup.ErrBadConnection {
+				e = r.(error)
+			} else {
+				panic(r)
+			}
+		}
+	}()
+
+	bucketName := args[0]
+	srcNamespace := args[1]
+	srcName := args[2]
+	namespace := args[3]
+	name := args[4]
+
+	restorePoint, err := time.Parse(constants.BackupTimeFormat, args[5])
+	if err != nil {
+		return fmt.Errorf("invalid restore point %s: %w", args[5], err)
+	}
+
+	b, err := makeBucket(bucketName)
+	if err != nil {
+		return fmt.Errorf("failed to create a bucket interface: %w", err)
+	}
+
+	cfg, err := ctrl.GetConfig()
+	if err != nil {
+		return fmt.Errorf("failed to get config for Kubernetes: %w", err)
+	}
+
+	rm, err := backup.NewRestoreManager(cfg, b, commonArgs.workDir,
+		srcNamespace, srcName,
+		namespace, name,
+		mysqlPassword,
+		commonArgs.threads,
+		restorePoint)
+	if err != nil {
+		return fmt.Errorf("failed to create a restore manager: %w", err)
+	}
+	return rm.Restore(cmd.Context())
 }
 
 func init() {
