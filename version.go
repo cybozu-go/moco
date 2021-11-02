190173ebf47c3d7857b79c3e8e02aa9dcd189500
Masayuki Ishii <masa213f@gmail.com>
2021-11-02 04:25:54 +0000

    Bump version to 0.10.7

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package moco
 
 const (
 	// Version is the MOCO version
-	Version = "0.10.6"
+	Version = "0.10.7"
 
 	// FluentBitImage is the image for slow-log sidecar container.
 	FluentBitImage = "quay.io/cybozu/fluent-bit:1.7.8.1"
