3c9026b7d9622431cf683cdf9008c5c180b92353
Masayuki Ishii <masa213f@gmail.com>
2021-12-21 07:10:50 +0000

    Update Fluent Bit container image
    Signed-off-by: Masayuki Ishii <masa213f@gmail.com>

--- version.go
+++ version.go
@@ -5,7 +5,7 @@ const (
 	Version = "0.10.9"
 
 	// FluentBitImage is the image for slow-log sidecar container.
-	FluentBitImage = "quay.io/cybozu/fluent-bit:1.7.8.1"
+	FluentBitImage = "quay.io/cybozu/fluent-bit:1.8.11.1"
 
 	// ExporterImage is the image for mysqld_exporter sidecar container.
 	ExporterImage = "quay.io/cybozu/mysqld_exporter:0.13.0.1"
