4c1d15b70016b04ad029d84e837a5021f9a04278
d-kuro <kurosawa7620@gmail.com>
2022-02-24 16:26:39 +0900

    Fixed panic when migrating to SSA. (#373)
    * Fixed panic when migrating to SSA.
    
    Signed-off-by: d-kuro <kurosawa7620@gmail.com>

--- controllers/mysql_container.go
+++ controllers/mysql_container.go
@@ -161,7 +161,9 @@ func (r *MySQLClusterReconciler) makeV1AgentContainer(cluster *mocov1beta2.MySQL
 }
 
 func (r *MySQLClusterReconciler) makeV1SlowQueryLogContainer(sts *appsv1ac.StatefulSetApplyConfiguration, force bool) *corev1ac.ContainerApplyConfiguration {
-	if !force {
+	stsINotNil := (sts != nil && sts.Spec != nil && sts.Spec.Template != nil && sts.Spec.Template.Spec != nil)
+
+	if !force && stsINotNil {
 		for _, c := range sts.Spec.Template.Spec.Containers {
 			if *c.Name == constants.SlowQueryLogAgentContainerName {
 				return &c
