9a306081c5544a82dd093315907c23b30ab407fa
d-kuro <kurosawa7620@gmail.com>
2022-04-27 04:14:54 +0900

    Check support volume expansion
    Signed-off-by: d-kuro <kurosawa7620@gmail.com>

--- controllers/resize_pvc.go
+++ controllers/resize_pvc.go
@@ -2,14 +2,21 @@ package controllers
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	mocov1beta2 "github.com/cybozu-go/moco/api/v1beta2"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	corev1ac "k8s.io/client-go/applyconfigurations/core/v1"
+	"k8s.io/utils/pointer"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	crlog "sigs.k8s.io/controller-runtime/pkg/log"
@@ -34,7 +41,7 @@ func (r *MySQLClusterReconciler) reconcilePVC(ctx context.Context, req ctrl.Requ
 		return nil
 	}
 
-	if isUpdatingStatefulSet(&sts) {
+	if r.isUpdatingStatefulSet(&sts) {
 		return nil
 	}
 
@@ -45,8 +52,13 @@ func (r *MySQLClusterReconciler) reconcilePVC(ctx context.Context, req ctrl.Requ
 
 	log.Info("Starting PVC resize")
 
-	if err := r.findAndResizePVC(ctx, cluster, &sts, resizeTarget); err != nil {
+	patches, err := r.findPVCs(ctx, cluster, &sts, resizeTarget)
+	if err != nil {
 		return fmt.Errorf("failed to resize PVC: %w", err)
+	}
+
+	if err := r.resizePVCs(ctx, patches); err != nil {
+		return fmt.Errorf("failed to resize PVCs: %w", err)
 	}
 
 	if err := r.deleteStatefulSet(ctx, &sts); err != nil {
@@ -56,7 +68,7 @@ func (r *MySQLClusterReconciler) reconcilePVC(ctx context.Context, req ctrl.Requ
 	return nil
 }
 
-func (r *MySQLClusterReconciler) findAndResizePVC(ctx context.Context, cluster *mocov1beta2.MySQLCluster, sts *appsv1.StatefulSet, resizeTarget map[string]corev1.PersistentVolumeClaim) error {
+func (r *MySQLClusterReconciler) findPVCs(ctx context.Context, cluster *mocov1beta2.MySQLCluster, sts *appsv1.StatefulSet, resizeTarget map[string]corev1.PersistentVolumeClaim) (map[types.NamespacedName]*unstructured.Unstructured, error) {
 	log := crlog.FromContext(ctx)
 
 	newSizes := make(map[string]*resource.Quantity)
@@ -79,13 +91,15 @@ func (r *MySQLClusterReconciler) findAndResizePVC(ctx context.Context, cluster *
 
 	selector, err := metav1.LabelSelectorAsSelector(sts.Spec.Selector)
 	if err != nil {
-		return fmt.Errorf("failed to parse selector: %w", err)
+		return nil, fmt.Errorf("failed to parse selector: %w", err)
 	}
 
 	var pvcs corev1.PersistentVolumeClaimList
 	if err := r.Client.List(ctx, &pvcs, client.MatchingLabelsSelector{Selector: selector}); err != nil {
-		return fmt.Errorf("failed to list PVCs: %w", err)
+		return nil, fmt.Errorf("failed to list PVCs: %w", err)
 	}
+
+	patches := make(map[types.NamespacedName]*unstructured.Unstructured)
 
 	for _, pvc := range pvcs.Items {
 		newSize, ok := pvcsToKeep[pvc.Name]
@@ -93,13 +107,50 @@ func (r *MySQLClusterReconciler) findAndResizePVC(ctx context.Context, cluster *
 			continue
 		}
 
-		pvc.Spec.Resources.Requests[corev1.ResourceStorage] = *newSize
-
-		if err := r.Client.Update(ctx, &pvc); err != nil {
-			return fmt.Errorf("failed to update PVC %s/%s: %w", pvc.Namespace, pvc.Name, err)
+		supported, err := r.isVolumeExpansionSupported(ctx, &pvc)
+		if err != nil {
+			return nil, fmt.Errorf("failed to check if volume expansion is supported: %w", err)
+		}
+		if !supported {
+			log.Info("StorageClass used by PVC does not support volume expansion, skipped", "storageClassName", *pvc.Spec.StorageClassName, "pvcName", pvc.Name)
+			continue
 		}
 
-		log.Info("Resized PVC", "pvcName", pvc.Name)
+		pvcac := corev1ac.PersistentVolumeClaim(pvc.Name, pvc.Namespace).
+			WithSpec(corev1ac.PersistentVolumeClaimSpec().
+				WithResources(corev1ac.ResourceRequirements().
+					WithRequests(corev1.ResourceList{corev1.ResourceStorage: *newSize}),
+				),
+			)
+
+		obj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(pvcac)
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert PVC %s/%s to unstructured: %w", pvc.Namespace, pvc.Name, err)
+		}
+
+		patches[types.NamespacedName{Namespace: pvc.Namespace, Name: pvc.Name}] = &unstructured.Unstructured{Object: obj}
+	}
+
+	if len(patches) == 0 {
+		return nil, errors.New("could not find resizable PVCs")
+	}
+
+	return patches, nil
+}
+
+func (r *MySQLClusterReconciler) resizePVCs(ctx context.Context, patches map[types.NamespacedName]*unstructured.Unstructured) error {
+	log := crlog.FromContext(ctx)
+
+	for key, patch := range patches {
+		err := r.Patch(ctx, patch, client.Apply, &client.PatchOptions{
+			FieldManager: fieldManager,
+			Force:        pointer.Bool(true),
+		})
+		if err != nil {
+			return fmt.Errorf("failed to patch PVC %s/%s: %w", key.Namespace, key.Name, err)
+		}
+
+		log.Info("Resized PVC", "pvcName", key.Name)
 	}
 
 	return nil
@@ -152,7 +203,24 @@ func (r *MySQLClusterReconciler) needResizePVC(cluster *mocov1beta2.MySQLCluster
 	return pvcSet, true
 }
 
-func isUpdatingStatefulSet(sts *appsv1.StatefulSet) bool {
+func (r *MySQLClusterReconciler) isVolumeExpansionSupported(ctx context.Context, pvc *corev1.PersistentVolumeClaim) (bool, error) {
+	if pvc.Spec.StorageClassName == nil {
+		return false, nil
+	}
+
+	var storageClass storagev1.StorageClass
+	if err := r.Client.Get(ctx, types.NamespacedName{Name: *pvc.Spec.StorageClassName}, &storageClass); err != nil {
+		return false, fmt.Errorf("failed to get StorageClass %s: %w", *pvc.Spec.StorageClassName, err)
+	}
+
+	if storageClass.AllowVolumeExpansion == nil {
+		return false, nil
+	}
+
+	return *storageClass.AllowVolumeExpansion, nil
+}
+
+func (_ MySQLClusterReconciler) isUpdatingStatefulSet(sts *appsv1.StatefulSet) bool {
 	if sts.Status.ObservedGeneration == 0 {
 		return false
 	}
