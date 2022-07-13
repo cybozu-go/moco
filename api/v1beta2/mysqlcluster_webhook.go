1912d5b491e93185b18fabd2cc80435cc757e334
d-kuro <kurosawa7620@gmail.com>
2022-07-13 11:14:17 +0900

    Use CustomDefaulter/CustomValidator for admission
    Signed-off-by: d-kuro <kurosawa7620@gmail.com>

--- api/v1beta2/mysqlcluster_webhook.go
+++ api/v1beta2/mysqlcluster_webhook.go
@@ -11,6 +11,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
@@ -18,57 +19,66 @@ import (
 func (r *MySQLCluster) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(r).
+		WithValidator(&mySQLClusterAdmission{client: mgr.GetAPIReader()}).
+		WithDefaulter(&mySQLClusterAdmission{client: mgr.GetAPIReader()}).
 		Complete()
+}
+
+type mySQLClusterAdmission struct {
+	client client.Reader
 }
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
 //+kubebuilder:webhook:path=/mutate-moco-cybozu-com-v1beta2-mysqlcluster,mutating=true,failurePolicy=fail,sideEffects=None,matchPolicy=Equivalent,groups=moco.cybozu.com,resources=mysqlclusters,verbs=create,versions=v1beta2,name=mmysqlcluster.kb.io,admissionReviewVersions=v1
 
-var _ webhook.Defaulter = &MySQLCluster{}
+var _ webhook.CustomDefaulter = &mySQLClusterAdmission{}
 
-// Default implements webhook.Defaulter so a webhook will be registered for the type
-func (r *MySQLCluster) Default() {
-	controllerutil.AddFinalizer(r, constants.MySQLClusterFinalizer)
+func (a *mySQLClusterAdmission) Default(ctx context.Context, obj runtime.Object) error {
+	cluster := obj.(*MySQLCluster)
 
-	if r.Spec.ServerIDBase == 0 {
+	controllerutil.AddFinalizer(cluster, constants.MySQLClusterFinalizer)
+
+	if cluster.Spec.ServerIDBase == 0 {
 		buf := make([]byte, 4) // server_id is a uint32 value
 		_, err := rand.Read(buf)
 		if err != nil {
 			panic(err)
 		}
-		r.Spec.ServerIDBase = int32(binary.LittleEndian.Uint32(buf)&uint32(math.MaxInt32>>1)) + 1
+		cluster.Spec.ServerIDBase = int32(binary.LittleEndian.Uint32(buf)&uint32(math.MaxInt32>>1)) + 1
 	}
+
+	return nil
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 //+kubebuilder:webhook:path=/validate-moco-cybozu-com-v1beta2-mysqlcluster,mutating=false,failurePolicy=fail,sideEffects=None,matchPolicy=Equivalent,groups=moco.cybozu.com,resources=mysqlclusters,verbs=create;update,versions=v1beta2,name=vmysqlcluster.kb.io,admissionReviewVersions=v1
 
-var _ webhook.Validator = &MySQLCluster{}
+var _ webhook.CustomValidator = &mySQLClusterAdmission{}
 
-// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
-func (r *MySQLCluster) ValidateCreate() error {
-	errs := r.Spec.validateCreate()
+func (a *mySQLClusterAdmission) ValidateCreate(ctx context.Context, obj runtime.Object) error {
+	cluster := obj.(*MySQLCluster)
+
+	errs := cluster.Spec.validateCreate()
 	if len(errs) == 0 {
 		return nil
 	}
 
-	return apierrors.NewInvalid(schema.GroupKind{Group: GroupVersion.Group, Kind: "MySQLCluster"}, r.Name, errs)
+	return apierrors.NewInvalid(schema.GroupKind{Group: GroupVersion.Group, Kind: "MySQLCluster"}, cluster.Name, errs)
 }
 
-// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
-func (r *MySQLCluster) ValidateUpdate(old runtime.Object) error {
-	ctx := context.Background()
+func (a *mySQLClusterAdmission) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) error {
+	oldCluster := oldObj.(*MySQLCluster)
+	newCluster := newObj.(*MySQLCluster)
 
-	errs := r.Spec.validateUpdate(ctx, apiReader, old.(*MySQLCluster).Spec)
+	errs := newCluster.Spec.validateUpdate(ctx, a.client, oldCluster.Spec)
 	if len(errs) == 0 {
 		return nil
 	}
 
-	return apierrors.NewInvalid(schema.GroupKind{Group: GroupVersion.Group, Kind: "MySQLCluster"}, r.Name, errs)
+	return apierrors.NewInvalid(schema.GroupKind{Group: GroupVersion.Group, Kind: "MySQLCluster"}, newCluster.Name, errs)
 }
 
-// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
-func (r *MySQLCluster) ValidateDelete() error {
+func (a *mySQLClusterAdmission) ValidateDelete(ctx context.Context, obj runtime.Object) error {
 	return nil
 }
