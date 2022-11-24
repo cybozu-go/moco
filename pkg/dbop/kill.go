559af60618ad42c4892e1799d7d6a0fd755d3044
Masayuki Ishii <masa213f@gmail.com>
2022-11-24 09:20:19 +0000

    reflect comment
    Signed-off-by: Masayuki Ishii <masa213f@gmail.com>

--- pkg/dbop/kill.go
+++ pkg/dbop/kill.go
@@ -2,6 +2,7 @@ package dbop
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/cybozu-go/moco/pkg/constants"
@@ -23,16 +24,18 @@ func (o *operator) KillConnections(ctx context.Context) error {
 			continue
 		}
 
-		if _, err := o.db.ExecContext(ctx, `KILL CONNECTION ?`, p.ID); err != nil && !isMySQLError(err, 1094) {
+		if _, err := o.db.ExecContext(ctx, `KILL CONNECTION ?`, p.ID); err != nil && !isNoSuchThread(err) {
 			return fmt.Errorf("failed to kill connection %d for %s from %s: %w", p.ID, p.User, p.Host, err)
 		}
 	}
 	return nil
 }
 
-func isMySQLError(err error, number uint16) bool {
-	if merr, ok := err.(*mysql.MySQLError); ok {
-		return merr.Number == number
+func isNoSuchThread(err error) bool {
+	var merr *mysql.MySQLError
+	// Error number 1094 is ER_NO_SUCH_THREAD.
+	if errors.As(err, &merr) && merr.Number == 1094 {
+		return true
 	}
 	return false
 }
