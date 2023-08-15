ef93aa716c972518b38eb4ecdafa9e29814f6685
yamatcha <soju-yamashita@cybozu.co.jp>
2023-08-15 07:16:12 +0000

    Updating mysqld_exporter and fluent-bit images used in moco

--- version.go
+++ version.go
@@ -5,8 +5,8 @@ const (
 	Version = "0.16.1"
 
 	// FluentBitImage is the image for slow-log sidecar container.
-	FluentBitImage = "ghcr.io/cybozu-go/moco/fluent-bit:2.0.9.1"
+	FluentBitImage = "ghcr.io/cybozu-go/moco/fluent-bit:2.1.8.2"
 
 	// ExporterImage is the image for mysqld_exporter sidecar container.
-	ExporterImage = "ghcr.io/cybozu-go/moco/mysqld_exporter:0.14.0.1"
+	ExporterImage = "ghcr.io/cybozu-go/moco/mysqld_exporter:0.15.0.2"
 )
