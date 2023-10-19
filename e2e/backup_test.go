d862e5d521c3adf2e31baea28a00c3f759c37428
kouki <kouworld0123@gmail.com>
2023-10-19 05:01:12 +0000

    fix test
    Signed-off-by: kouki <kouworld0123@gmail.com>

--- e2e/backup_test.go
+++ e2e/backup_test.go
@@ -41,6 +41,13 @@ var _ = Context("backup", func() {
 
 	It("should construct a source cluster", func() {
 		kubectlSafe(fillTemplate(backupYAML), "apply", "-f", "-")
+		secjson := kubectlSafe(nil, "get", "secret", "-o", "json", "minio-cert")
+		sec := &corev1.Secret{}
+		json.Unmarshal(secjson, sec)
+		sec.SetNamespace("backup")
+		secjson, err := json.Marshal(sec)
+		Expect(err).NotTo(HaveOccurred())
+		kubectlSafe(secjson, "apply", "-f", "-")
 		Eventually(func() error {
 			cluster, err := getCluster("backup", "source")
 			if err != nil {
