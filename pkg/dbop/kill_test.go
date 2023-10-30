30a41f2cd6af4f75fe6467708e2a018344f4f6a0
Masayuki Ishii <masa.ishii.1019@gmail.com>
2023-10-30 18:24:23 +0900

    Revert "Fix flaky test"

--- pkg/dbop/kill_test.go
+++ pkg/dbop/kill_test.go
@@ -10,13 +10,6 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-type process struct {
-	ID    uint64 `db:"ID"`
-	User  string `db:"USER"`
-	Host  string `db:"HOST"`
-	State string `db:"STATE"` // for debugging
-}
-
 var _ = Describe("kill", func() {
 	It("should kill non-system processes only", func() {
 		By("preparing a single node cluster")
@@ -41,32 +34,35 @@ var _ = Describe("kill", func() {
 		defer db.Close()
 
 		By("getting process list")
-		var procs []process
-		err = op.(*operator).db.Select(&procs, `SELECT ID, USER, HOST, STATE FROM information_schema.PROCESSLIST`)
+		var procs []Process
+		err = op.(*operator).db.Select(&procs, `SELECT ID, USER, HOST FROM information_schema.PROCESSLIST`)
 		Expect(err).NotTo(HaveOccurred())
 
+		fooFound := false
 		for _, p := range procs {
-			fmt.Printf("process %d for %s from %s: %s\n", p.ID, p.User, p.Host, p.State)
+			fmt.Printf("process %d for %s from %s\n", p.ID, p.User, p.Host)
+			if p.User == "foo" {
+				fooFound = true
+			}
 		}
-		Expect(procs).To(ContainElement(HaveField("User", "foo")))
+		Expect(fooFound).To(BeTrue())
 
 		By("killing user process")
 		err = op.KillConnections(context.Background())
 		Expect(err).NotTo(HaveOccurred())
 
-		Eventually(func(g Gomega) {
-			var procs2 []process
-			err := op.(*operator).db.Select(&procs2, `SELECT ID, USER, HOST, STATE FROM information_schema.PROCESSLIST`)
-			g.Expect(err).NotTo(HaveOccurred())
+		var procs2 []Process
+		err = op.(*operator).db.Select(&procs2, `SELECT ID, USER, HOST FROM information_schema.PROCESSLIST`)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(procs) - len(procs2)).To(Equal(1))
 
-			// For debugging, print process list before confirming.
-			for _, p := range procs2 {
-				fmt.Printf("process %d for %s from %s: %s\n", p.ID, p.User, p.Host, p.State)
+		fooFound = false
+		for _, p := range procs2 {
+			fmt.Printf("process %d for %s from %s\n", p.ID, p.User, p.Host)
+			if p.User == "foo" {
+				fooFound = true
 			}
-			fmt.Println("")
-
-			g.Expect(procs2).To(HaveLen(len(procs) - 1))
-			g.Expect(procs2).NotTo(ContainElement(HaveField("User", "foo")))
-		}).Should(Succeed())
+		}
+		Expect(fooFound).To(BeFalse())
 	})
 })
