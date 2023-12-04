1432febd9aa3999e0a3a8e657900fdc3c8b6720e
Soju Yamashita <soju-yamashita@cybozu.co.jp>
2023-12-04 18:27:13 +0900

    Upgrade fluent-bit container to 2.2.0.1 (#618)

--- version.go
+++ version.go
@@ -5,7 +5,7 @@ const (
 	Version = "0.19.0"
 
 	// FluentBitImage is the image for slow-log sidecar container.
-	FluentBitImage = "ghcr.io/cybozu-go/moco/fluent-bit:2.1.8.2"
+	FluentBitImage = "ghcr.io/cybozu-go/moco/fluent-bit:2.2.0.1"
 
 	// ExporterImage is the image for mysqld_exporter sidecar container.
 	ExporterImage = "ghcr.io/cybozu-go/moco/mysqld_exporter:0.15.0.2"
