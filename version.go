559190992f30f162e08fd959a0629e78e8224d53
yamatcha <soju-yamashita@cybozu.co.jp>
2023-12-15 05:34:16 +0000

    Bump version to 0.20.0

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package moco
 
 const (
 	// Version is the MOCO version
-	Version = "0.19.0"
+	Version = "0.20.0"
 
 	// FluentBitImage is the image for slow-log sidecar container.
 	FluentBitImage = "ghcr.io/cybozu-go/moco/fluent-bit:2.2.0.1"
