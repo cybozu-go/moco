403eb1a5d36e6e7daef4559dcc138318a5e85a31
vas <v.sliouniaev@gmail.com>
2024-04-13 15:14:23 +0100

    Fix init container arguments

--- controllers/mysql_container.go
+++ controllers/mysql_container.go
@@ -128,10 +128,10 @@ func (r *MySQLClusterReconciler) makeV1AgentContainer(cluster *mocov1beta2.MySQL
 		WithImage(r.AgentImage)
 
 	if cluster.Spec.MaxDelaySeconds != nil {
-		c.WithArgs("--max-delay", fmt.Sprintf("%ds", *cluster.Spec.MaxDelaySeconds))
+		c.WithArgs(fmt.Sprintf("--max-delay=%ds", *cluster.Spec.MaxDelaySeconds))
 	}
 	if cluster.Spec.LogRotationSchedule != "" {
-		c.WithArgs("--log-rotation-schedule", cluster.Spec.LogRotationSchedule)
+		c.WithArgs(fmt.Sprintf("--log-rotation-schedule=%s", cluster.Spec.LogRotationSchedule))
 	}
 
 	c.WithVolumeMounts(
