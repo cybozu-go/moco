19431120200f0218963516791cdc1658a7d826fd
yamatcha <soju-yamashita@cybozu.co.jp>
2025-09-02 01:13:35 +0000

    Update sidecar image

--- version.go
+++ version.go
@@ -5,8 +5,8 @@ const (
 	Version = "0.28.0"
 
 	// FluentBitImage is the image for slow-log sidecar container.
-	FluentBitImage = "ghcr.io/cybozu-go/moco/fluent-bit:3.1.7.1"
+	FluentBitImage = "ghcr.io/cybozu-go/moco/fluent-bit:4.0.8.1"
 
 	// ExporterImage is the image for mysqld_exporter sidecar container.
-	ExporterImage = "ghcr.io/cybozu-go/moco/mysqld_exporter:0.16.0.1"
+	ExporterImage = "ghcr.io/cybozu-go/moco/mysqld_exporter:0.17.2.1"
 )
