8c56e0e9397a260ba8dc37bd306e90e23592ece0
shunki-fujita <shunki-fujita@cybozu.co.jp>
2026-07-17 07:27:37 +0000

    Pass ctx to MySQLClusterReconciler.SetupWithManager in test
    The signature grew a context.Context parameter in #924
    (Prevent MySQLCluster controller startup slowdown in large clusters);
    this branch's test setup was on the pre-#924 signature.
    
    Signed-off-by: shunki-fujita <shunki-fujita@cybozu.co.jp>

--- controllers/credentialrotation_controller_test.go
+++ controllers/credentialrotation_controller_test.go
@@ -93,7 +93,7 @@ var _ = Describe("CredentialRotation reconciler", func() {
 			ExporterImage:              testExporterImage,
 			MySQLConfigMapHistoryLimit: 2,
 		}
-		err = mysqlr.SetupWithManager(mgr)
+		err = mysqlr.SetupWithManager(ctx, mgr)
 		Expect(err).ToNot(HaveOccurred())
 
 		credRotr := &CredentialRotationReconciler{
